7d3bf5e7498fe02a590986f26aeffd8b30ec778c
jihopark <jihodayo@gmail.com>
2023-09-21 00:30:13 +0900

    design: 아이콘 추가 및 컬러 설정
    번개볼링 아이콘 추가 및 커스텀 컬러 설정 완료

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        Lightning_orange: '#FE7E07',
+        Lightning_yellow: '#FFDE67',
+        'custom-gradient': 'bg-gradient-to-br from-[#FE7E07] to-[#FFDE67]',
+      },
+
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
