dedc267e2a2c60f3d9e16908b76ec21d44e10be2
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-09-21 17:31:32 +0900

    feat: PostCard 컴포넌트 prop으로 내부 데이터 수정
    더미 데이터를 이용하여 PostCard 컴포넌트 내부의 데이터 수정
    현재 참가 인원에 대한 API 명세가 작성되어 있지 않아 추후 수정 필요

--- src/components/molecules/PostCard/index.tsx
+++ src/components/molecules/PostCard/index.tsx
@@ -26,44 +26,44 @@ function PostCard({
   return (
     <section className="post-card flex flex-col gap-6 p-7 rounded-2xl shadow">
       <div className="post-title-upper">
-        <Badge isRecruiting={true} />
+        <Badge isClose={isClose} />
         <span className="text-sm mx-2">
           <MdPeopleAlt className="inline mr-1 text-neutral-400" />
           <span>참석 </span>
-          <span className="text-[#37D629]">{1}</span>
+          <span className="text-[#37D629]">{"hard"}</span>
         </span>
         <span className="text-neutral-400">
           <span className="mr-1">모집마감</span>
-          <span>{"9월 7일 (목) 오후 9:00 - hard coded"}</span>
+          <span>{dueTime}</span>
         </span>
       </div>
       <div className="post-title-wrapper">
-        <h1 className="post-title text-2xl">
-          {"이번 주 저녁에 부산대 락볼링장에서 게임하실분~~-hard coded"}
-        </h1>
+        <h1 className="post-title text-2xl">{title}</h1>
       </div>
       <div className="post-title-lower flex w-full justify-between">
         <div className="post-info-wrapper text-neutral-400">
           <p className="district-name">
             <MdLocationPin className="inline" />
-            <span>{"부산광역시 금정구 장전2동-hard"}</span>
+            <span>{districtName}</span>
           </p>
           <p className="start-time">
             <MdAlarm className="inline" />
-            <span>{"9월 9일 (토) 오전 9:00-hard"}</span>
+            <span>{startTime}</span>
           </p>
         </div>
         <div className="user-profile flex items-center h-10">
           {/* // Image 컴포넌트로 외부 경로 이미지 로드시 next.config.js를 설정해 줘야해서 일단 img 태그로 구현 */}
-          {/* <Image alt="유저 프로필 이미지" src={"이미지url"} fill={true}/> */}
+          {/* <Image alt="유저 프로필 이미지" src={profileImage ? `임시APIURL${profileImage}` : "기본이미지경로"} fill={true}/> */}
           <img
             alt="유저 프로필 이미지"
             src={
-              "https://w7.pngwing.com/pngs/741/68/png-transparent-user-computer-icons-user-miscellaneous-cdr-rectangle-thumbnail.png"
+              profileImage
+                ? `임시APIURL${profileImage}`
+                : "https://w7.pngwing.com/pngs/741/68/png-transparent-user-computer-icons-user-miscellaneous-cdr-rectangle-thumbnail.png"
             }
             className="user-profile-image rounded-full h-full"
           />
-          <span className="user-name text-2xl mx-1">{"닉네임"}</span>
+          <span className="user-name text-2xl mx-1">{userName}</span>
         </div>
       </div>
     </section>
