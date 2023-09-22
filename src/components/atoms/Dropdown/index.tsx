7d59629c4621566395fa65ec5e32c1fb55e6bf14
jihopark <jihodayo@gmail.com>
2023-09-22 19:38:46 +0900

    feat: 드롭다운 기능 추가
    지역기반 탐색을 위한 드롭다운 기능 추가

--- src/components/atoms/Dropdown/index.tsx
+++ src/components/atoms/Dropdown/index.tsx
@@ -1,6 +1,6 @@
-"use client";
+'use client';
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 interface Option {
   value: string;
@@ -11,77 +11,87 @@ interface DropdownProps {
   options: Option[];
   selectedOption: string;
   onChange: (selectedValue: string) => void;
+  dropdownId: string; // Unique identifier for the dropdown
 }
 
-export default function Dropdown({ options, selectedOption, onChange }: DropdownProps) {
+export default function Dropdown(props: DropdownProps) {
+  const options = props.options;
+  const selectedOption = props.selectedOption;
+  const onChange = props.onChange;
+  const dropdownId = props.dropdownId;
+
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleDropdown = () => {
+  function toggleDropdown() {
     setIsOpen(!isOpen);
-  };
+  }
 
-  const handleOptionClick = (value: string) => {
+  function handleOptionClick(value: string) {
     onChange(value);
     setIsOpen(false);
-  };
+  }
 
   useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
+    function handleClickOutside(event: MouseEvent) {
       if (isOpen) {
-        const dropdown = document.getElementById("dropdown");
+        const dropdown = document.getElementById(dropdownId);
         if (dropdown && !dropdown.contains(event.target as Node)) {
           setIsOpen(false);
         }
       }
-    };
+    }
 
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener('mousedown', handleClickOutside);
 
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, [isOpen]);
+  }, [isOpen, dropdownId]);
 
   return (
-    <div id='dropdown' className='relative inline-block text-left'>
-      <div className='relative'>
+    <div id={dropdownId} className="relative inline-block text-left">
+      <div className="relative">
         <button
           onClick={toggleDropdown}
-          type='button'
-          className='inline-flex justify-between w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-opacity-75 focus-visible:ring-blue-300'
-          id='options-menu'
-          aria-haspopup='true'
+          type="button"
+          className="inline-flex justify-between w-40 px-4 py-2 text-sm font-medium text-border_color bg-white border border-border_color rounded-full hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-opacity-75 focus-visible:ring-blue-300"
+          id="options-menu"
+          aria-haspopup="true"
           aria-expanded={isOpen}
         >
           {selectedOption}
           <svg
-            className={`w-5 h-5 ml-2 transition-transform transform ${isOpen ? "rotate-180" : ""}`}
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 20 20'
-            fill='currentColor'
-            aria-hidden='true'
+            className={`w-5 h-5 ml-2 transition-transform transform ${isOpen ? 'rotate-180' : ''}`}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
           >
             <path
-              fillRule='evenodd'
-              d='M6.293 5.293a1 1 0 011.414 0L10 8.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z'
-              clipRule='evenodd'
+              fillRule="evenodd"
+              d="M6.293 5.293a1 1 0 011.414 0L10 8.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+              clipRule="evenodd"
             />
           </svg>
         </button>
       </div>
 
       {isOpen && (
-        <div className='absolute right-0 mt-2 bg-white border border-gray-300 rounded-md shadow-lg origin-top-right ring-1 ring-black ring-opacity-5'>
-          <ul className='py-1'>
-            {options.map((option) => (
-              <li
-                key={option.value}
-                onClick={() => handleOptionClick(option.value)}
-                className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'
-              >
-                {option.label}
-              </li>
-            ))}
+        <div className="absolute right-0 mt-2 bg-white border border-gray-300 rounded-md shadow-lg origin-top-right ring-1 ring-black ring-opacity-5">
+          <ul className="py-1">
+            {options.map(function (option) {
+              return (
+                <li
+                  key={option.value}
+                  onClick={function () {
+                    handleOptionClick(option.value);
+                  }}
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                >
+                  {option.label}
+                </li>
+              );
+            })}
           </ul>
         </div>
       )}
---
+++ src/components/organisms/SignupModal/index.tsx
@@ -0,0 +1,117 @@
+'use client';
+
+import React, { useState } from 'react';
+import Image from 'next/image';
+import Button from '@/components/atoms/Button';
+import Input from '@/components/atoms/Input';
+import Dropdown from '@/components/atoms/Dropdown';
+
+export default function Box() {
+  const createOption = (value: string) => ({ value, label: value });
+
+  const options1 = [createOption('광역시 / 도'), createOption('서울특별시'), createOption('부산광역시')];
+
+  const options2 = [createOption('시 / 군 / 구'), createOption('남구'), createOption('북구')];
+
+  const options3 = [createOption('동 / 면 / 읍'), createOption('장전동'), createOption('부저동')];
+
+  const [selectedOption1, setSelectedOption1] = useState(options1[0].value);
+  const [selectedOption2, setSelectedOption2] = useState(options2[0].value);
+  const [selectedOption3, setSelectedOption3] = useState(options3[0].value);
+
+  const handleDropdownChange1 = (selectedValue: any) => {
+    setSelectedOption1(selectedValue);
+  };
+
+  const handleDropdownChange2 = (selectedValue: any) => {
+    setSelectedOption2(selectedValue);
+  };
+
+  const handleDropdownChange3 = (selectedValue: any) => {
+    setSelectedOption3(selectedValue);
+  };
+
+  return (
+    <div className="">
+      <div className="flex items-center justify-center pb-[44px]">
+        <Image src="/logo.png" width={100} height={100} alt="logo" />
+        <h1 className="text-[60px] text-transparent bg-clip-text bg-thunder">번개볼링</h1>
+      </div>
+
+      <div>
+        {/* 이메일 입력란 */}
+        <div className="mb-4">
+          <Input
+            id="email"
+            type="email"
+            placeholder="이메일"
+            className="w-full py-2 px-3 rounded-lg border border-gray-400"
+          />
+        </div>
+        <p className="pb-[32px]" />
+        {/* 닉네임 입력란 */}
+        <div className="mb-4">
+          <Input
+            id="nickname"
+            type="text"
+            placeholder="닉네임"
+            className="w-full py-2 px-3 rounded-lg border border-gray-400"
+          />
+        </div>
+        <p className="pb-[32px]" />
+        {/* 비밀번호 입력란 */}
+        <div className="mb-4">
+          <Input
+            id="password"
+            type="password"
+            placeholder="비밀번호"
+            className="w-full py-2 px-3 rounded-lg border border-gray-400"
+          />
+        </div>
+        <p className="pb-[32px]" />
+        {/* 비밀번호 확인 입력란 */}
+        <div className="mb-4">
+          <Input
+            id="confirmPassword"
+            type="password"
+            placeholder="비밀번호 확인"
+            className="w-full py-2 px-3 rounded-lg border border-gray-400"
+          />
+        </div>
+      </div>
+
+      {/* 지역 선택 */}
+      <div className="flex justify-between">
+        <Dropdown
+          options={options1}
+          selectedOption={selectedOption1}
+          onChange={handleDropdownChange1}
+          dropdownId="dropdown1" // Provide a unique identifier
+        />
+
+        <Dropdown
+          options={options2}
+          selectedOption={selectedOption2}
+          onChange={handleDropdownChange2}
+          dropdownId="dropdown2" // Provide a unique identifier
+        />
+
+        <Dropdown
+          options={options3}
+          selectedOption={selectedOption3}
+          onChange={handleDropdownChange3}
+          dropdownId="dropdown3" // Provide a unique identifier
+        />
+      </div>
+
+      {/* 제출 버튼 */}
+      <div className="flex justify-center ">
+        {Button(
+          'submit',
+          'bg-gradient-to-tl from-[#FE7E07] to-[#FFDE67] text-white w-[546px] h-[60px] rounded-full',
+          '회원가입'
+        )}
+      </div>
+    </div>
+  );
+}
