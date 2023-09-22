47da68e43214d16211b0ca43897a38611ab63acf
jihopark <jihodayo@gmail.com>
2023-09-22 22:22:31 +0900

    design: 모달 ui 변경
    고정된 크기에서 컴포넌트 크기에 따라 변경되도록 수정

--- src/components/molecules/Modal/index.tsx
+++ src/components/molecules/Modal/index.tsx
@@ -33,11 +33,8 @@ export default function Modal({ children }: { children: React.ReactNode }) {
   }, [onKeyDown]);
 
   return (
-    <div
-      ref={overlay}
-      className="fixed z-10 left-0 right-0 top-0 bottom-0 flex items-center justify-center bg-black/60"
-    >
-      <div ref={wrapper} className="relative w-[546px] h-[795px] bg-white rounded-lg shadow-lg p-6">
+    <div ref={overlay} className="fixed z-10 left-0 right-0 top-0 bottom-0 flex items-center justify-center ">
+      <div ref={wrapper} className="relative bg-white rounded-2xl p-10 border-[#868484] shadow-2xl">
         <button
           className="absolute top-2 right-2 text-gray-400 hover:text-gray-600 text-2xl font-bold rounded-full w-10 h-10 flex items-center justify-center bg-gray-200 hover:bg-gray-300 focus:outline-none"
           onClick={onDismiss}
