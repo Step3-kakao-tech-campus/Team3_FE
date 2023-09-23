98da0c1561e2eaeccb6c71929830340feda2afde
kjh302903 <kjh302903@gmail.com>
2023-09-23 18:14:14 +0900

    feat: NavigationBar 컴포넌트 추가
    웹페이지 GNB 컴포넌트 추가

---
+++ src/components/molecules/NavigationBar/index.tsx
@@ -0,0 +1,32 @@
+import InnerContainer from "@/components/atoms/InnerContainer";
+import Image from "next/image";
+import Logo from "/public/images/bowling_logo.png";
+import Link from "next/link";
+import Button from "@/components/atoms/Button";
+
+function NavigationBar() {
+  return (
+    <nav className="fixed top-0 inset-x-0 z-999 bg-white">
+      <InnerContainer>
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center space-x-4">
+            <div className="flex items-center mr-4">
+              <Image src={Logo} alt="볼링 로고" width={45} height={45} />
+              <span className="text-2xl font-bold bg-clip-text bg-gradient-to-tl from-lightningYellow to-lightningOrange text-transparent">
+                번개볼링
+              </span>
+            </div>
+            <Link href="/">홈</Link>
+            <Link href="/">볼링장 찾기</Link>
+          </div>
+          <div className="flex items-center space-x-4">
+            <Button style={"white"}>로그인</Button>
+            <Button style={"lightning"}>회원가입</Button>
+          </div>
+        </div>
+      </InnerContainer>
+    </nav>
+  );
+}
+
+export default NavigationBar;
