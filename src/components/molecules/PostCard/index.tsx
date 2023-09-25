0f7e2bab9e15b438bf992d8c2cde1e19edbdbf1f
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-09-25 17:11:55 +0900

    design: PostCard 컴포넌트 hover시 scale 효과 적용
    PostCard 컴포넌트에 hover시 scale 효과를 적용했다.
    또한, scale시 transition 효과를 주어 자연스러운 효과를 적용했다.
    scale 비율은 103%로 설정했다.

--- src/components/molecules/PostCard/index.tsx
+++ src/components/molecules/PostCard/index.tsx
@@ -22,7 +22,7 @@ interface Props {
 
 function PostCard({ data }: Props) {
   return (
-    <section className="post-card flex flex-col gap-6 p-7 rounded-2xl shadow">
+    <section className="post-card flex flex-col gap-6 p-7 rounded-2xl shadow hover:scale-[103%] transition">
       <div className="post-title-upper">
         <Badge isClose={data.isClose} />
         <span className="text-sm mx-2">
