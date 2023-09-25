68ef535f93baa559baecc43b0203100b948e45aa
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-09-25 11:45:38 +0900

    feat: PostCard props 타입 객체화
    PostCard로 전달되는 prop을 객체화 하였다.
    새로운 자료형 PostData를 정의하여 내부에 기존에 사용되던 prop들을 넣어주었다.
    PostCard에서 사용되는 Prop은 PostData를 자료형으로 한다.

--- src/components/molecules/PostCard/index.tsx
+++ src/components/molecules/PostCard/index.tsx
@@ -4,7 +4,8 @@ import { MdPeopleAlt } from "react-icons/md";
 import Badge from "@/components/atoms/Badge";
 import Image from "next/image";
 
-interface Props {
+export interface PostData {
+  id: number;
   title: string;
   dueTime: string;
   startTime: string;
@@ -15,57 +16,52 @@ interface Props {
   currentNumber: number;
 }
 
-function PostCard({
-  title,
-  dueTime,
-  startTime,
-  districtName,
-  userName,
-  profileImage,
-  isClose,
-  currentNumber,
-}: Props) {
+interface Props {
+  data: PostData;
+}
+
+function PostCard({ data }: Props) {
   return (
     <section className="post-card flex flex-col gap-6 p-7 rounded-2xl shadow">
       <div className="post-title-upper">
-        <Badge isClose={isClose} />
+        <Badge isClose={data.isClose} />
         <span className="text-sm mx-2">
           <MdPeopleAlt className="inline mr-1 text-neutral-400" />
           <span>참석 </span>
-          <span className="text-[#37D629]">{currentNumber}</span>
+          <span className="text-[#37D629]">{data.currentNumber}</span>
         </span>
         <span className="text-neutral-400">
           <span className="mr-1">모집마감</span>
-          <span>{dueTime}</span>
+          <span>{data.dueTime}</span>
         </span>
       </div>
       <div className="post-title-wrapper">
-        <h1 className="post-title text-2xl">{title}</h1>
+        <h1 className="post-title text-2xl">{data.title}</h1>
       </div>
       <div className="post-title-lower flex w-full justify-between">
         <div className="post-info-wrapper text-neutral-400">
           <p className="district-name">
             <MdLocationPin className="inline" />
-            <span>{districtName}</span>
+            <span>{data.districtName}</span>
           </p>
           <p className="start-time">
             <MdAlarm className="inline" />
-            <span>{startTime}</span>
+            <span>{data.startTime}</span>
           </p>
         </div>
         <div className="user-profile flex items-center h-10">
           {/* // Image 컴포넌트로 외부 경로 이미지 로드시 next.config.js를 설정해 줘야해서 일단 img 태그로 구현 */}
-          {/* <Image alt="유저 프로필 이미지" src={profileImage ? `임시APIURL${profileImage}` : "기본이미지경로"} fill={true}/> */}
+          {/* <Image alt="유저 프로필 이미지" src={data.profileImage ? `임시APIURL${profileImage}` : "기본이미지경로"} fill={true}/> */}
           <img
             alt="유저 프로필 이미지"
             src={
-              profileImage
-                ? `임시APIURL${profileImage}`
+              data.profileImage
+                ? `임시APIURL${data.profileImage}`
                 : "https://w7.pngwing.com/pngs/741/68/png-transparent-user-computer-icons-user-miscellaneous-cdr-rectangle-thumbnail.png"
             }
             className="user-profile-image rounded-full h-full"
           />
-          <span className="user-name text-2xl mx-1">{userName}</span>
+          <span className="user-name text-2xl mx-1">{data.userName}</span>
         </div>
       </div>
     </section>
