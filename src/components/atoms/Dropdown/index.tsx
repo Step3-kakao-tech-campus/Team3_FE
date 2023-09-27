d04ac8769582e3cfd66cfc4864b31711f3e71a11
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-09-28 00:57:34 +0900

    feat: Dropdown 컴포넌트 prop추가
    Dropdown 컴포넌트에 placeholder prop을 추가하여 placeholder처럼 표시할 수 있도록 했다.

--- src/components/atoms/Dropdown/index.tsx
+++ src/components/atoms/Dropdown/index.tsx
@@ -1,22 +1,29 @@
+"use client";
+
 interface Props {
-  options: { value: string; label: string }[];
-  selectedValue: string;
+  options: { name: string; id: number }[];
   onChange: React.ChangeEventHandler<HTMLSelectElement>;
   className?: string;
+  placeholder: string;
 }
 
-function Dropdown({ options, selectedValue, onChange, className }: Props) {
+function Dropdown({ options, onChange, className, placeholder }: Props) {
   return (
     <div className={`flex flex-col space-y-2 ${className}`}>
       <select
-        value={selectedValue}
         onChange={onChange}
         className="border rounded-full p-2"
         style={{ borderRadius: "20px", padding: "4px", minWidth: "90px" }}
       >
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
+        <option key="placeholder" value={-1} disabled hidden selected>
+          {placeholder}
+        </option>
+        <option key="select_all" value={-1}>
+          전체
+        </option>
+        {options?.map((option) => (
+          <option key={option.id} value={option.id}>
+            {option.name}
           </option>
         ))}
       </select>
