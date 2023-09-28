1740df19bcf00af37000fc9d18ab40b03383674d
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-09-28 23:06:46 +0900

    feat: PostFilter 컴포넌트 생성
    PostFilter 컴포넌트를 생성했다.

---
+++ src/components/molecules/PostFilter/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+function PostFilter() {
+  return (
+    <div className="post-filter flex w-fit">
+      <button type="button">전체 보기</button>
+      <button type="button">모집중</button>
+    </div>
+  );
+}
+
+export default PostFilter;
