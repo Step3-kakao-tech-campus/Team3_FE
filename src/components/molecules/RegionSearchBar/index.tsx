8d81dca5f43fc95a635d292022437bd8e6a9beb2
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-09-28 21:37:26 +0900

    feat: RegionSearchBar 버튼 기능 구현
    검색 버튼 클릭시 설정한 지역을 search param으로 하는 경로로 이동하도록 했다.

--- src/components/molecules/RegionSearchBar/index.tsx
+++ src/components/molecules/RegionSearchBar/index.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import objectToQueryString from "@/utils/objectToQueryString";
 import DropdownBox from "../DropdownBox";
 
 function RegionSearchBar() {
@@ -9,15 +11,22 @@ function RegionSearchBar() {
     countryId: "-1",
     districtId: "-1",
   });
+  const router = useRouter();
+  const queryString = objectToQueryString(regionIds);
   return (
-    <div className="region-search-bar flex">
+    <div className="region-search-bar flex items-center">
       <DropdownBox
         styleType="big"
         onChange={(newIds) => {
           setRegionIds(newIds);
         }}
       />
-      <button type="button" onClick={() => {}}>
+      <button
+        type="button"
+        onClick={() => {
+          router.push(`/${queryString}`);
+        }}
+      >
         검색
       </button>
     </div>
