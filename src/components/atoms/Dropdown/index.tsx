7dde647bb8d700b7dc56481dd1e26a9bd929f8c4
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-09-29 00:57:06 +0900

    fix: Dropdown 컴포넌트 기본값 설정 로직 변경
    selectedOptionId가 있을 경우 기본값을 설정하는 로직을 변경했다.
    기존에 <option> 내부에서 selected 옵션을 주던 방식에서 브라우저 콘솔 에러가 발생하여
    selected 옵션을 주지 않고 value를 selectedOptionId로 설정하는 방식으로 변경하였다.

--- src/components/atoms/Dropdown/index.tsx
+++ src/components/atoms/Dropdown/index.tsx
@@ -14,23 +14,20 @@ function Dropdown({ options, onChange, placeholder, styleType, selectedOptionId 
     big: "rounded-3xl p-5 min-w-[230px] text-2xl text-center",
   };
   return (
-    <select onChange={onChange} className={`border text-neutral-500 ${styleObj[styleType]}`} defaultValue={-1}>
-      <option key="placeholder" value={-1} disabled hidden>
+    <select
+      onChange={onChange}
+      className={`border text-neutral-500 ${styleObj[styleType]}`}
+      value={selectedOptionId || "-1"}
+    >
+      <option key="placeholder" value="-1" hidden>
         {placeholder}
       </option>
       {placeholder !== "광역시 / 도" && (
-        <option key="select_all" value={-1}>
+        <option key="select_all" value="0">
           전체
         </option>
       )}
       {options?.map((option) => {
-        if (selectedOptionId === option.id.toString()) {
-          return (
-            <option key={option.id} value={option.id} selected>
-              {option.name}
-            </option>
-          );
-        }
         return (
           <option key={option.id} value={option.id}>
             {option.name}
