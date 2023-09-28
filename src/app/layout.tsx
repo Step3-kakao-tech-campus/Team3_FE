4a5c5f2e2515a60fdedfe4dc7ef340ec31491a9b
jihopark <jihodayo@gmail.com>
2023-09-29 01:48:41 +0900

    style: 폰트 설정
    메인 layout에서 사용할 폰트 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,14 @@ import "@/styles/globals.css";
 import type { Metadata } from "next";
 import Background from "@/components/atoms/Background";
 import NavigationBar from "@/components/molecules/NavigationBar";
+import { Noto_Sans_KR } from "next/font/google";
+
+const notoSans = Noto_Sans_KR({
+  weight: ["100", "300", "400", "500", "700", "900"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Malgun Gothic", "Roboto", "sans-serif"],
+});
 
 export const metadata: Metadata = {
   title: "Thunder bowling",
@@ -15,7 +23,7 @@ export default function RootLayout({ children, modal }: { children: React.ReactN
         <link rel="icon" href="/favicons/favicon.ico" />
       </head>
 
-      <body className="bg-[#F6F6F6]">
+      <body className={`bg-[#F6F6F6] ${notoSans.className}`}>
         <NavigationBar />
         <Background>{children}</Background>
         {modal}
