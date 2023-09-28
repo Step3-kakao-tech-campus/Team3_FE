d37a03dc99d8cd46f945d8def10fcbbd30c173e6
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-09-28 23:15:37 +0900

    feat: PostFilter 기능 개발
    두 개의 버튼 각각 클릭시 적절한 query parameter를 추가한 이후 router.push 하도록 구현했다.

--- src/components/molecules/PostFilter/index.tsx
+++ src/components/molecules/PostFilter/index.tsx
@@ -1,10 +1,42 @@
 "use client";
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+
 function PostFilter() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const newSearchParams = new URLSearchParams(searchParams);
+  const router = useRouter();
+
+  const searchParamAll = newSearchParams.get("all");
+  const [isAll, setIsAll] = useState(searchParamAll !== "false");
+
+  const handleOnClick = (newIsAll: boolean) => {
+    setIsAll(newIsAll);
+    newSearchParams.set("all", newIsAll.toString());
+    const queryString = newSearchParams.toString();
+    router.push(`${pathname}?${queryString}`);
+  };
+
   return (
     <div className="post-filter flex w-fit">
-      <button type="button">전체 보기</button>
-      <button type="button">모집중</button>
+      <button
+        type="button"
+        onClick={() => {
+          handleOnClick(true);
+        }}
+      >
+        전체 보기
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          handleOnClick(false);
+        }}
+      >
+        모집중
+      </button>
     </div>
   );
 }
