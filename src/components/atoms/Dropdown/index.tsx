e935ba9e0ca8dd78435bad61aca2bca52e8209c9
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-09-28 23:46:49 +0900

    feat: Dropdown selectedOptionId prop 추가
    Dropdown 컴포넌트에 selectedOptionId prop을 추가했다.
    selectedOptionId는 refresh 등으로 상태를 잃고 난 이후에도 선택되어 있는 옵션을 원할 때 이용한다.

--- src/components/atoms/Dropdown/index.tsx
+++ src/components/atoms/Dropdown/index.tsx
@@ -5,9 +5,10 @@ interface Props {
   onChange: React.ChangeEventHandler<HTMLSelectElement>;
   placeholder: string;
   styleType: "big" | "small";
+  selectedOptionId?: string;
 }
 
-function Dropdown({ options, onChange, placeholder, styleType }: Props) {
+function Dropdown({ options, onChange, placeholder, styleType, selectedOptionId }: Props) {
   const styleObj = {
     small: "rounded-full p-1 min-w-[130px]",
     big: "rounded-3xl p-5 min-w-[230px] text-2xl text-center",
@@ -22,11 +23,20 @@ function Dropdown({ options, onChange, placeholder, styleType }: Props) {
           전체
         </option>
       )}
-      {options?.map((option) => (
-        <option key={option.id} value={option.id}>
-          {option.name}
-        </option>
-      ))}
+      {options?.map((option) => {
+        if (selectedOptionId === option.id.toString()) {
+          return (
+            <option key={option.id} value={option.id} selected>
+              {option.name}
+            </option>
+          );
+        }
+        return (
+          <option key={option.id} value={option.id}>
+            {option.name}
+          </option>
+        );
+      })}
     </select>
   );
 }
