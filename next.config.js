89e62bcabeb425c200329a37772fb2c95ac9a0f4
jihopark <jihodayo@gmail.com>
2023-10-01 19:32:38 +0900

    docs: StrictMode 비 활성화
    useEffect등 개발자 모드에서 처음 렌더링을 포함해 두번씩 동작하는걸 줄임

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  reactStrictMode: false,
+};
 
 module.exports = nextConfig;
