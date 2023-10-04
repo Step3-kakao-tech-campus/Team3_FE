9b3512139a3fe556ffb99816ea28a98d27c6c374
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-04 16:01:44 +0900

    feat: ApplicantBlock UI 구성
    ApplicantBlock의 UI를 구성했다.

--- src/components/molecules/ApplicantBlock/index.tsx
+++ src/components/molecules/ApplicantBlock/index.tsx
@@ -1,5 +1,34 @@
-function ApplicantBlock() {
-  return <div>ApplicantBlock</div>;
+import Button from "@/components/atoms/Button";
+import CircularProfileImage from "@/components/atoms/CircularProfileImage";
+
+interface UserInfo {
+  name: string;
+  profileImage: string;
+  rating: number;
+}
+
+function ApplicantBlock({ name, profileImage, rating }: UserInfo) {
+  return (
+    <div className="applicant flex items-center justify-between border rounded-2xl py-2 px-4">
+      <div className="user-info flex gap-2 items-center">
+        <CircularProfileImage src={profileImage} className="w-10 h-10" />
+        <div className="user-info-text flex flex-col justify-start">
+          <span className="block w-fit font-bold text-slate-700">{name}</span>
+          <span className="block w-fit text-sm text-neutral-500">{`매너점수 ★ ${
+            rating ? `${rating}/5` : "없음"
+          }`}</span>
+        </div>
+      </div>
+      <div className="confirm-control flex gap-3">
+        <Button styleType="outlined_gray_full_sm">
+          <span className="block text-sm font-normal min-w-[40px] leading-none fontsize">마감</span>
+        </Button>
+        <Button styleType="thunder_full_sm">
+          <span className="block text-sm font-normal min-w-[40px]">수락</span>
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default ApplicantBlock;
