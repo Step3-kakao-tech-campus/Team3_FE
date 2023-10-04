46ecfa4b0a9751973078d0e8b9f0ade98a32b0d2
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-04 11:07:24 +0900

    docs: CircularProfileImage JSdoc 수정
    CircularProfileImage의 JSdoc을 수정했다.

--- src/components/atoms/CircularProfileImage/index.tsx
+++ src/components/atoms/CircularProfileImage/index.tsx
@@ -5,6 +5,11 @@ interface Props {
   className?: string;
 }
 
+/**
+ * 원형 유저 프로필 이미지 컴포넌트.
+ * className으로 tailwind 키워드를 전달받을 수 있어, 추후 반응형 스타일 적용이 원활하도록 했다.
+ * 기본 사이즈는 32*32px이다.
+ */
 function CircularProfileImage({ src, className = "h-8 w-8" }: Props) {
   return (
     <div className={`profile-image rounded-full overflow-hidden ${className}`}>
