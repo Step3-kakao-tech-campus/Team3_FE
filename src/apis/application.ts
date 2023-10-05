eab75a9ab90aa13e15006155a0164bc7024b882f
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-06 00:04:34 +0900

    feat: postAcceptApplicant 신청 수락 함수 정의
    /apis/application.ts에 신청 수락 함수 postAcceptApplicant를 정의하였다.

--- src/apis/application.ts
+++ src/apis/application.ts
@@ -1,7 +1,16 @@
 import client from "./instance";
 
-export default async function getApplicants(postId: number) {
+export async function getApplicants(postId: number) {
   if (!postId) throw new Error("postId 또는 accessToken이 유효하지 않습니다.");
   const res = await client.get(`/api/posts/${postId}/applicants?size=100`);
+  return res;
+}
+
+export async function postAcceptApplicant(postId: number, applicantId: number) {
+  if (!postId) throw new Error("postAcceptApplicant: postId 또는 accessToken이 유효하지 않습니다.");
+  if (!applicantId) throw new Error("postAcceptApplicant: applicantId가 유효하지 않습니다.");
+  const res = await client.post(`/api/posts/${postId}/applicants/${applicantId}`, {
+    status: true,
+  });
   return res;
 }
