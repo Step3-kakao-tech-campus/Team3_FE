0a63a3e72028a0b591a24ba164c0684a68e88b0e
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-06 00:56:11 +0900

    feat: ApplicantBlock 수락/거절 기능 구현
    ApplicantBlock 컴포넌트에 수락 / 거절 버튼의 기능을 구현했다.

--- src/components/molecules/ApplicantBlock/index.tsx
+++ src/components/molecules/ApplicantBlock/index.tsx
@@ -1,14 +1,36 @@
+import { deleteRejectApplicant, putAcceptApplicant } from "@/apis/application";
 import Button from "@/components/atoms/Button";
 import CircularProfileImage from "@/components/atoms/CircularProfileImage";
+import { useCallback } from "react";
 
 interface ApplicantData {
   name: string;
   profileImage: string;
   rating: number;
   postId: number;
+  applicantId: number;
 }
 
-function ApplicantBlock({ name, profileImage, rating, postId }: ApplicantData) {
+function ApplicantBlock({ name, profileImage, rating, postId, applicantId }: ApplicantData) {
+  const handleAcceptReject = useCallback(
+    (type: "accept" | "reject") => {
+      if (type === "accept") {
+        try {
+          putAcceptApplicant(postId, applicantId);
+        } catch {
+          alert("수락 요청이 실패했습니다.");
+        }
+      } else {
+        try {
+          deleteRejectApplicant(postId, applicantId);
+        } catch {
+          alert("거절 요청이 실패했습니다.");
+        }
+      }
+    },
+    [applicantId, postId],
+  );
+
   return (
     <div className="applicant flex items-center justify-between border rounded-2xl py-2 px-4">
       <div className="user-info flex gap-2 items-center">
@@ -21,10 +43,10 @@ function ApplicantBlock({ name, profileImage, rating, postId }: ApplicantData) {
         </div>
       </div>
       <div className="confirm-control flex gap-3">
-        <Button styleType="outlined_gray_full_sm">
-          <span className="block text-sm font-normal min-w-[40px] leading-none fontsize">마감</span>
+        <Button styleType="outlined_gray_full_sm" onClick={() => handleAcceptReject("reject")}>
+          <span className="block text-sm font-normal min-w-[40px] leading-none fontsize">거절</span>
         </Button>
-        <Button styleType="thunder_full_sm">
+        <Button styleType="thunder_full_sm" onClick={() => handleAcceptReject("accept")}>
           <span className="block text-sm font-normal min-w-[40px]">수락</span>
         </Button>
       </div>
