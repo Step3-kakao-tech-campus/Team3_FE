4afa21f5c74c6f49414d69b3d340315bd2024691
kjh302903 <kjh302903@gmail.com>
2023-10-05 19:30:23 +0900

    feat: 게시글 상세정보 fetcher 함수 추가
    게시글 상세정보 fetcher 함수 추가
    posts.ts >> getPostById

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -3,7 +3,7 @@ import { getCookie } from "@/utils/Cookie";
 
 const client = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: 1000,
+  timeout: 3000,
   headers: {
     "Content-Type": "application/json",
   },
