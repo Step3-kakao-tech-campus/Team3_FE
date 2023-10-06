f4ed55a5ec932ce95b6afda14b741043097958a9
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-06 17:31:03 +0900

    feat: ProfileModalTemplate 프로필 조회 api 적용
    ProfileModalTemplate에 프로필 조회 api를 적용했다.

--- src/components/templates/ProfileModalTemplate/index.tsx
+++ src/components/templates/ProfileModalTemplate/index.tsx
@@ -1,24 +1,37 @@
 "use client";
 
+import { getProfileById } from "@/apis/profile";
 import CircularProfileImage from "@/components/atoms/CircularProfileImage";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
 import { MdLocationPin, MdMail, MdLeaderboard } from "react-icons/md";
 
 function ProfileModalTemplate() {
   const buttonStyle = "min-w-[150px] p-2 mx-auto flex gap-2 justify-center items-center  bg-neutral-200 rounded-full";
+  const pageParam = useParams();
+  const { data, isLoading, isError } = useQuery({
+    queryFn: () => getProfileById(parseInt(pageParam.user_id as string, 10)),
+  });
+
+  const user = data?.data?.response;
+
   return (
     <div className="profile-modal-contents">
-      <div className="profile-modal-upper flex gap-5 bg-thunder p-7 items-center">
-        <CircularProfileImage src={null} styleType="huge" />
+      <div className="profile-modal-upper flex gap-5 bg-thunder px-10 py-7 items-center">
+        <CircularProfileImage src={user?.profileImage} styleType="huge" />
         <div className="user-info text-white flex flex-col gap-1">
           <div className="flex justify-between items-center">
-            <span className="text-lg font-semibold">볼링조아</span>
-            <span className="text-sm">Average 100</span>
+            <span className="text-lg font-semibold">{user?.name}</span>
+            <span className="text-sm">{`Average ${user?.averageScore}`}</span>
           </div>
           <p>
             <MdLocationPin className="inline" />
-            <span>부산광역시 부산진구 부전동</span>
+            <span>{user?.address}</span>
           </p>
-          <p>{`매너점수 ★ ${"user.rating " ? `${"user.rating"}/5` : "없음"}`}</p>
+          <div className="rating flex justify-between items-center">
+            <span>매너점수</span>
+            <span>{`★ ${user?.rating ? `${user?.rating}/5` : "없음"}`}</span>
+          </div>
           {/* 내 프로필이라면 내 정보 수정 페이지 Link 추가 */}
         </div>
       </div>
