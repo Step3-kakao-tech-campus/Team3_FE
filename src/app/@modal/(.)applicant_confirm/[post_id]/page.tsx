a05bf02ad1b6fe983c731f319e9c1925eab1d535
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-06 10:39:16 +0900

    feat: applicant_confirm 모달 페이지 생성
    applicant_confirm 모달 페이지를 생성했다.

---
+++ src/app/@modal/(.)applicant_confirm/[post_id]/page.tsx
@@ -0,0 +1,12 @@
+import Modal from "@/components/atoms/Modal";
+import ApplicantConfirmTemplate from "@/components/templates/ApplicantConfirmTemplate";
+
+function ApplicantConfirmPage() {
+  return (
+    <Modal>
+      <ApplicantConfirmTemplate />
+    </Modal>
+  );
+}
+
+export default ApplicantConfirmPage;
