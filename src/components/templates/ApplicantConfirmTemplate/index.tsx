4cbc33bf5fd161fc772e75335d16f0f165b57202
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-06 11:38:16 +0900

    feat: ApplicantConfirmTempalte 에러시 마감하기 버튼 미출력
    ApplicantConfirmTempalte 에러시 마감하기 버튼을 보이지 않도록 했다.

--- src/components/templates/ApplicantConfirmTemplate/index.tsx
+++ src/components/templates/ApplicantConfirmTemplate/index.tsx
@@ -61,10 +61,12 @@ function ApplicantConfirmTemplate() {
   return (
     <div className="applicant-confirm flex flex-col gap-4 text-center w-[500px]">
       <h1 className="text-2xl">신청자 확인</h1>
-      <div className="applicant-number-with-button flex justify-between items-center">
-        <span>총 {response?.participantNumber}건의 신청 요청이 있습니다.</span>
-        <Button styleType="thunder_full_sm">마감하기</Button>
-      </div>
+      {!isError && (
+        <div className="applicant-number-with-button flex justify-between items-center">
+          <span>총 {response?.participantNumber}건의 신청 요청이 있습니다.</span>
+          <Button styleType="thunder_full_sm">마감하기</Button>
+        </div>
+      )}
       <div className="applicant-list flex flex-col gap-3 max-h-[400px] min-h-[100px] overflow-y-auto">
         {renderComponent()}
       </div>
