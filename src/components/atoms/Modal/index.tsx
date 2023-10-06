1a7e3166db34801c69a10698e6ab846187745308
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-06 16:32:47 +0900

    feat: Modal 컴포넌트 noPadding prop 추가
    Modal 컴포넌트에 noPadding prop을 추가했다.
    noPadding 옵션을 true로 설정 시 내부 패딩이 없도록 스타일을 적용했다.

--- src/components/atoms/Modal/index.tsx
+++ src/components/atoms/Modal/index.tsx
@@ -6,9 +6,10 @@ import { MdClose } from "react-icons/md";
 
 interface Props {
   children: React.ReactNode;
+  noPadding?: boolean;
 }
 
-function Modal({ children }: Props) {
+function Modal({ children, noPadding }: Props) {
   const overlay = useRef(null);
   const wrapper = useRef(null);
   const router = useRouter();
@@ -31,7 +32,10 @@ function Modal({ children }: Props) {
 
   return (
     <div ref={overlay} className="fixed z-10 left-0 right-0 top-0 bottom-0 flex items-center justify-center ">
-      <div ref={wrapper} className="relative bg-white rounded-2xl p-6 border-[#868484] shadow-2xl">
+      <div
+        ref={wrapper}
+        className={`relative bg-white rounded-2xl border-[#868484] shadow-2xl ${noPadding ? "p-0" : "p-6"}`}
+      >
         <button
           type="button"
           className="absolute top-2 right-2 text-gray-400 hover:text-gray-600 text-2xl font-bold rounded-full w-7 h-7 flex items-center justify-center focus:outline-none"
