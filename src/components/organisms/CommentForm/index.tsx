e4035abc7b0559219ee6dcea8f99f6d4a87ebc29
kjh302903 <kjh302903@gmail.com>
2023-10-07 23:42:08 +0900

    feat: 댓글에 무한스크롤 적용
    댓글에 무한스크롤 적용

--- src/components/organisms/CommentForm/index.tsx
+++ src/components/organisms/CommentForm/index.tsx
@@ -2,13 +2,52 @@
 
 import { MdSend } from "react-icons/md";
 import RoundImage from "@/components/atoms/RoundImage";
-import React from "react";
+import React, { useCallback, useEffect, useMemo, useRef } from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import getComments from "@/apis/comment";
+import Comment, { CommentWithChild } from "@/components/molecules/Comment";
 
 interface Props {
   id: string;
 }
 
 function CommentForm({ id }: Props) {
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    ["/comments", id],
+    ({ pageParam = null }) => getComments(id, pageParam),
+    {
+      retry: false,
+      getNextPageParam: (lastPage) => {
+        const newKey = lastPage?.data?.response?.nextCursorRequest?.key;
+        return newKey !== -1 ? newKey : undefined;
+      },
+    },
+  );
+
+  const target = useRef<HTMLDivElement>(null);
+
+  const handleIntersect = useCallback(
+    async ([entry]: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      if (entry.isIntersecting) {
+        observer.unobserve(entry.target);
+        if (hasNextPage) {
+          await fetchNextPage();
+        }
+        observer.observe(entry.target);
+      }
+    },
+    [fetchNextPage, hasNextPage],
+  );
+
+  const observer = useMemo(() => new IntersectionObserver(handleIntersect), [handleIntersect]);
+
+  useEffect(() => {
+    if (target.current) {
+      observer.observe(target.current);
+    }
+    return () => observer && observer.disconnect();
+  }, [target, data, observer]);
+
   return (
     <div>
       <h2 className="mt-4 text-xl">댓글</h2>
@@ -21,6 +60,15 @@ function CommentForm({ id }: Props) {
         />
         <MdSend size="36" className="text-neutral-400 cursor-pointer" />
       </div>
+      <div className="mt-6">
+        {data?.pages?.map(
+          (page) =>
+            page?.data?.response?.comments.map((comment: CommentWithChild) => (
+              <Comment comment={comment} key={comment.id} />
+            )),
+        )}
+      </div>
+      {hasNextPage && <div className="observe-area" ref={target} />}
     </div>
   );
 }
