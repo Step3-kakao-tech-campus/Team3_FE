95a6359a5690ab4af51515241c335e688d0e2c59
kjh302903 <kjh302903@gmail.com>
2023-10-07 20:57:11 +0900

    refactor: rounded prop 리팩토링
    rounded 클래스네임을 동적으로 사용하던 방법 수정

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -15,10 +15,15 @@ function Button({ styleType, rounded, size, onClick, children }: Props) {
     lg: "w-[546px] h-[40px] text-xl",
     sm: "",
   };
+  const roundedObj = {
+    full: "rounded-full",
+    md: "rounded-md",
+  };
+
   return (
     <button
       type="button"
-      className={`px-2 py-1 ring-gray-400 ring-inset font-bold filter hover:brightness-95 ${styleObj[styleType]} rounded-${rounded} ${sizeObj[size]}`}
+      className={`px-2 py-1 ring-gray-400 ring-inset font-bold filter hover:brightness-95 ${styleObj[styleType]} ${roundedObj[rounded]} ${sizeObj[size]}`}
       onClick={onClick}
     >
       {children}
