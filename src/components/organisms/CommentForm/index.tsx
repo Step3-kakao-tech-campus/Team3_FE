d9205919c58ed98aaca651213be1ddb31e695dfd
kjh302903 <kjh302903@gmail.com>
2023-10-08 15:32:01 +0900

    feat: 댓글 조회, 추가 기능 추가
    댓글 조회 추가 기능 추가
    댓글 조회는 무한스크롤 적용
    댓글 추가는 mutation으로 추가 함

--- src/components/organisms/CommentForm/index.tsx
+++ src/components/organisms/CommentForm/index.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { MdSend } from "react-icons/md";
 import RoundImage from "@/components/atoms/RoundImage";
 import React, { useCallback, useEffect, useMemo, useRef } from "react";
 import { useInfiniteQuery } from "@tanstack/react-query";
-import getComments from "@/apis/comment";
+import { getComments } from "@/apis/comment";
 import Comment, { CommentWithChild } from "@/components/molecules/Comment";
+import CommentSubmit from "@/components/molecules/CommentSubmit";
+import useCommentsMutation from "@/hooks/useCommentsMutation";
 
 interface Props {
   id: string;
@@ -24,6 +25,9 @@ function CommentForm({ id }: Props) {
     },
   );
 
+  const { mutate, queryClient } = useCommentsMutation();
+
+  const commentRef = useRef<HTMLTextAreaElement>(null);
   const target = useRef<HTMLDivElement>(null);
 
   const handleIntersect = useCallback(
@@ -39,6 +43,22 @@ function CommentForm({ id }: Props) {
     [fetchNextPage, hasNextPage],
   );
 
+  const handleSubmit = () => {
+    const payload = {
+      id,
+      content: commentRef.current!.value,
+    };
+    mutate(payload, {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["/comments", id]);
+        commentRef.current!.value = "";
+      },
+      onError: (error) => {
+        console.log(error);
+      },
+    });
+  };
+
   const observer = useMemo(() => new IntersectionObserver(handleIntersect), [handleIntersect]);
 
   useEffect(() => {
@@ -53,12 +73,7 @@ function CommentForm({ id }: Props) {
       <h2 className="mt-4 text-xl">댓글</h2>
       <div className="mt-6 flex items-center justify-between">
         <RoundImage alt="유저 프로필 이미지" src="/images/default_profile_image.png" className="w-10 h-10" />
-        <input
-          type="text"
-          placeholder="내용을 입력해 주세요."
-          className="w-5/6 py-2 px-3 rounded-lg border border-gray-400"
-        />
-        <MdSend size="36" className="text-neutral-400 cursor-pointer" />
+        <CommentSubmit commentRef={commentRef} onClick={handleSubmit} />
       </div>
       <div className="mt-6">
         {data?.pages?.map(
