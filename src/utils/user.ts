f3c4f0c6ecd45d06fdfb65389b329dc0374cff5b
kjh302903 <kjh302903@gmail.com>
2023-10-08 22:45:30 +0900

    feat: cookie에exp, userId 추가
    쿠키에 exp, userId 추가 하도록 변경

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,19 +1,26 @@
 import { removeCookie, setCookie } from "@/utils/Cookie";
 
-export const setLogin = async (email: string, token: string) => {
-  setCookie("email", email, { maxAge: 3600 * 168 });
-  setCookie("token", token, { maxAge: 3600 * 168 });
-};
-
-export const deleteToken = async () => {
-  removeCookie("email");
-  removeCookie("token");
-};
-
 export const getTokenPayload = (token: string) => {
   const Parts = token.split(".")[1];
   const decoded = atob(Parts);
   const payload = JSON.parse(decoded);
 
   return payload;
+};
+
+export const setLogin = async (email: string, token: string) => {
+  const payload = getTokenPayload(token);
+  const maxAge = 3600 * 168;
+
+  setCookie("email", email, { maxAge });
+  setCookie("token", token, { maxAge });
+  setCookie("exp", payload.exp, { maxAge });
+  setCookie("userId", payload.sub, { maxAge });
+};
+
+export const deleteToken = async () => {
+  removeCookie("email");
+  removeCookie("token");
+  removeCookie("exp");
+  removeCookie("userId");
 };
