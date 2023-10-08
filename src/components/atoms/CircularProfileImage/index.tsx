8b5727196201cfdbd04a41dbfd5e443871491bfd
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-08 14:03:51 +0900

    docs: CircularProfileImage JSDoc 수정
    CircularProfileImage JSDoc 수정

--- src/components/atoms/CircularProfileImage/index.tsx
+++ src/components/atoms/CircularProfileImage/index.tsx
@@ -12,8 +12,12 @@ interface Props {
 
 /**
  * 원형 유저 프로필 이미지 컴포넌트.
- * className으로 tailwind 키워드를 전달받을 수 있어, 추후 반응형 스타일 적용이 원활하도록 했다.
- * 기본 사이즈는 32*32px이다.
+ * styleType으로 크기를 전달받는다
+ * huge: "w-20 h-20",
+ * lg: "w-10 h-10",
+ * md: "w-8 h-8",
+ * sm: "w-6 h-6",
+ * 기본 사이즈는 'md': 32*32px이다.
  */
 function CircularProfileImage({ src, styleType = "md" }: Props) {
   const styles = {
