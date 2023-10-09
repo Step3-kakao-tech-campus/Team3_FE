a050437bc51046897dffe077f0c6b4ba13912c41
kjh302903 <kjh302903@gmail.com>
2023-10-09 10:32:29 +0900

    feat: 댓글 삭제 기능 추가
    댓글 삭제 기능 추가

--- src/components/molecules/Comment/index.tsx
+++ src/components/molecules/Comment/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { MdOutlineEdit, MdOutlineDelete } from "react-icons/md";
 import { CommentData } from "@/types/commentData";
 import CircularProfileImage from "@/components/atoms/CircularProfileImage";
+import { getCookie } from "@/utils/Cookie";
+import useMutateWithQueryClient from "@/hooks/useMutateWithQueryClient";
+import { deleteComments } from "@/apis/comment";
 import ChildComment from "../ChildComment";
 
 export interface CommentWithChild extends CommentData {
@@ -9,9 +13,29 @@ export interface CommentWithChild extends CommentData {
 
 interface Props {
   comment: CommentWithChild;
+  id: number;
 }
 
-function Comment({ comment }: Props) {
+function Comment({ comment, id }: Props) {
+  const userId = parseInt(getCookie("userId"), 10);
+
+  const { mutate, queryClient } = useMutateWithQueryClient(deleteComments);
+
+  const handleDeleteComment = () => {
+    const payload = {
+      postId: id,
+      commentId: comment.id,
+    };
+    mutate(payload, {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["/comments", id]);
+      },
+      onError: (error) => {
+        console.log(error);
+      },
+    });
+  };
+
   return (
     <>
       <div className="flex items-center gap-3">
@@ -19,8 +43,20 @@ function Comment({ comment }: Props) {
         <div className="flex-1">
           <div className="flex items-center justify-between">
             <span className="text-[#2a5885]">{comment.userName}</span>
-            <div className="text-neutral-400">
-              <span className="text-sm">답글 달기</span>
+            <div className="flex items-center gap-2 text-neutral-400 text-sm">
+              {comment.userId === userId && (
+                <>
+                  <span className="flex items-center cursor-pointer">
+                    <MdOutlineEdit />
+                    수정
+                  </span>
+                  <button type="button" onClick={handleDeleteComment} className="flex items-center cursor-pointer">
+                    <MdOutlineDelete />
+                    삭제
+                  </button>
+                </>
+              )}
+              <span>답글 달기</span>
             </div>
           </div>
           <pre className="whitespace-pre-wrap break-all">{comment.content}</pre>
