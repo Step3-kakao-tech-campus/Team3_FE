b5ec552190f99768c31abdc2657e864457a2534e
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-10 01:30:09 +0900

    feat: 스코어보드 페이지 생성
    스코어보드 페이지를 /app/scoreboard/[user_id] 디렉토리에 생성했다.

---
+++ src/app/scoreboard/[user_id]/page.tsx
@@ -0,0 +1,12 @@
+import BackArrowContainer from "@/components/atoms/BackArrowContainer";
+import ScoreboardTemplate from "@/components/templates/ScoreboardTemplate";
+
+function ScoreboardPage() {
+  return (
+    <BackArrowContainer>
+      <ScoreboardTemplate />
+    </BackArrowContainer>
+  );
+}
+
+export default ScoreboardPage;
