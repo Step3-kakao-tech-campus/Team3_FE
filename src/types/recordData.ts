e54123a24411e3b86a2b5e8f1559dcccfdd49acd
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-09 19:49:34 +0900

    feat: 사용자 정의 타입 RecordData 정의
    사용자 정의 타입 RecordData를 /types/recordData.ts에 정의했다.

---
+++ src/types/recordData.ts
@@ -0,0 +1,20 @@
+export interface RecordData {
+  id: number;
+  title: string;
+  dueTime: Date;
+  districtName: string;
+  startTime: Date;
+  currentNumber: number;
+  isClose: boolean;
+  scores: {
+    id: number;
+    score: number;
+    scoreImage: string | null;
+  }[];
+  members: {
+    id: number;
+    name: string;
+    profileImage: string | null;
+    isRated: boolean;
+  }[];
+}
