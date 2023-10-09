cc7cdf1becee72eb077a9c66c17c7ba0c38a3e0a
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-10 01:29:36 +0900

    feat: ScoreboardTemplate 임시 구현
    ScoreboardTemplate을 임시 구현하였다.

--- src/components/templates/ScoreboardTemplate/index.tsx
+++ src/components/templates/ScoreboardTemplate/index.tsx
@@ -1,5 +1,66 @@
+import RecordSummary from "@/components/atoms/RecordSummary";
+import RecordCard from "@/components/molecules/RecordCard";
+import RecordFilter from "@/components/molecules/RecordFilter";
+import RecordSearchBar from "@/components/molecules/RecordSearchBar";
+
 function ScoreboardTemplate() {
-  return <div>스코어보드템플릿</div>;
+  const dummy = {
+    id: 2,
+    title: "오늘 7시에 부산대 락볼링장에서 게임하실분~~",
+    dueTime: "2023-09-07T21:00:00",
+    districtName: "부산광역시 금정구 장전2동",
+    startTime: "2023-09-09T19:00:00",
+    currentNumber: 1,
+    isClose: true,
+    scores: [
+      {
+        id: 1,
+        score: 180,
+        scoreImage: "/score-images/1.jpg",
+      },
+      {
+        id: 2,
+        score: 210,
+        scoreImage: null,
+      },
+    ],
+    members: [
+      {
+        id: 2,
+        name: "최볼링",
+        profileImage: null,
+        isRated: true,
+      },
+      {
+        id: 3,
+        name: "이볼링",
+        profileImage: null,
+        isRated: false,
+      },
+      {
+        id: 4,
+        name: "최볼링",
+        profileImage: null,
+        isRated: true,
+      },
+      {
+        id: 1,
+        name: "김볼링",
+        profileImage: null,
+        isRated: false,
+      },
+    ],
+  };
+  return (
+    <div className="scoreboard flex flex-col gap-5">
+      <h1 className="title text-2xl">{"김볼링"}님의 기록</h1>
+      <RecordSummary game={20} average={160} maximum={180} minimum={110} />
+      <h2 className="title text-xl">참여 기록</h2>
+      <RecordFilter />
+      <RecordSearchBar />
+      <RecordCard data={dummy} />
+    </div>
+  );
 }
 
 export default ScoreboardTemplate;
