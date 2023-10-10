8e3c443a014089052b32aad2752f6a6ec0fab97d
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-10 22:44:56 +0900

    refactor: BackArrowContainer BackArrowButton 적용
    BackArrowContainer에  BackArrowButton 적용하여 'use client'를 삭제했다.

--- src/components/atoms/BackArrowContainer/index.tsx
+++ src/components/atoms/BackArrowContainer/index.tsx
@@ -1,21 +1,13 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import { MdArrowBack } from "react-icons/md";
+import BackArrowButton from "../BackArrowButton";
 
 interface Props {
   children: React.ReactNode;
 }
 
 function BackArrowContainer({ children }: Props) {
-  const router = useRouter();
-  const handleBack = () => {
-    router.refresh();
-    router.push("/");
-  };
   return (
     <div className="mt-8 p-16 bg-white lg:mx-28">
-      <MdArrowBack onClick={handleBack} size="30" className="cursor-pointer" />
+      <BackArrowButton />
       {children}
     </div>
   );
