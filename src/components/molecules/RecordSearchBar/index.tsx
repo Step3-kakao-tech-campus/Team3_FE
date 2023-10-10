af511154119c46d7ad80039b47f1ade1fa3e04fd
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-10 19:52:43 +0900

    feat: RecordSearchBar api 연결 및 handleOnChange 정의
    RecordSearchBar 컴포넌트에서 cityId 조회 api 연결 및 handleOnChange 정의하여 Dropdown에 적용했다.

--- src/components/molecules/RecordSearchBar/index.tsx
+++ src/components/molecules/RecordSearchBar/index.tsx
@@ -1,16 +1,52 @@
 "use client";
 
+import { getCities } from "@/apis/district";
 import Button from "@/components/atoms/Button";
 import Dropdown from "@/components/atoms/Dropdown";
+import { useQuery } from "@tanstack/react-query";
+import { useCallback, useState } from "react";
 
 function RecordSearchBar(): JSX.Element {
+  const [searchBarState, setSearchBarState] = useState({ cityId: 0, start: "2022-02", end: "2022-02" });
+  const { data } = useQuery({
+    queryKey: ["cityId"],
+    queryFn: getCities,
+  });
+
+  const handleOnChange = useCallback(
+    (event: React.ChangeEvent<HTMLSelectElement>, dropdownType: "cityId" | "start" | "end") => {
+      const newState = dropdownType === "cityId" ? parseInt(event.target.value, 10) : event.target.value;
+      setSearchBarState((prev) => ({
+        ...prev,
+        [dropdownType]: newState,
+      }));
+    },
+    [],
+  );
+
+  const cities = data?.data?.response?.cities ?? [];
   return (
     <div className="record-search-bar flex gap-4">
-      <Dropdown placeholder="" onChange={() => {}} styleType="small" options={[{ id: 0, name: "전체 지역" }]} />
+      <Dropdown
+        placeholder=""
+        onChange={(e) => handleOnChange(e, "cityId")}
+        styleType="small"
+        options={[{ id: 0, name: "전체 지역" }, ...cities]}
+      />
       <div className="period-dropdown flex gap-2 items-center">
-        <Dropdown placeholder="" onChange={() => {}} styleType="small" options={[{ id: 0, name: "1111년 11월" }]} />
+        <Dropdown
+          placeholder=""
+          onChange={(e) => handleOnChange(e, "start")}
+          styleType="small"
+          options={[{ id: 0, name: "1111년 11월" }]}
+        />
         <span className="leading-none">~</span>
-        <Dropdown placeholder="" onChange={() => {}} styleType="small" options={[{ id: 0, name: "9999년 9월" }]} />
+        <Dropdown
+          placeholder=""
+          onChange={(e) => handleOnChange(e, "end")}
+          styleType="small"
+          options={[{ id: 0, name: "9999년 9월" }]}
+        />
       </div>
       <Button styleType="thunder" fontWeight="normal" size="sm" rounded="full">
         조회
