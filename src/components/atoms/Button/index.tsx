c75e124a0d4535246869a099bdde933d8ca8aaf7
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-12 00:47:50 +0900

    refactor: Button 컴포넌트 prop 수정
    Button 컴포넌트 prop을 수정했다.
    noLineHeight, fontSize = "md", padding = "px-2_py-1", minWidth Prop을 삭제했다.
    styleType에 thunder-w-20을 추가했다.
    size에 xs를 추가했다.

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,40 +1,34 @@
 export interface ButtonProps {
-  styleType: "white" | "thunder" | "outlined-gray" | "outlined-orange" | "outlined-blue" | "filled-blue";
+  styleType:
+    | "white"
+    | "thunder"
+    | "thunder-w-20"
+    | "outlined-gray"
+    | "outlined-orange"
+    | "outlined-blue"
+    | "filled-blue";
   rounded: "full" | "md";
-  size: "lg" | "sm";
+  size: "lg" | "sm" | "xs";
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   children: React.ReactNode;
-  noLineHeight?: boolean;
   fontWeight?: "bold" | "semibold" | "normal";
-  fontSize?: "sm" | "md" | "lg";
-  padding?: "px-2_py-1" | "px-2_py-[3px]";
-  minWidth?: "70px";
 }
 
-function Button({
-  styleType,
-  rounded,
-  size,
-  onClick,
-  children,
-  noLineHeight,
-  fontWeight = "bold",
-  fontSize = "md",
-  padding = "px-2_py-1",
-  minWidth,
-}: ButtonProps) {
+function Button({ styleType, rounded, size, onClick, children, fontWeight = "bold" }: ButtonProps) {
   const commonStyle = "ring-gray-400 ring-inset filter hover:brightness-95";
   const styleObj = {
     white: "text-gray-600 ring-1 bg-white",
     thunder: "bg-thunder text-white",
-    "outlined-gray": "border border-neutral-400 text-neutral-400 bg-white",
-    "outlined-orange": `border border-thunderOrange text-thunderOrange bg-white`,
-    "outlined-blue": `border border-blue-400 text-blue-400 bg-white`,
-    "filled-blue": `border text-white bg-blue-500`,
+    "thunder-w-20": "bg-thunder text-white min-w-[80px]",
+    "outlined-gray": "border border-neutral-400 text-neutral-400 bg-white min-w-[80px]",
+    "outlined-orange": `border border-thunderOrange text-thunderOrange bg-white min-w-[80px]`,
+    "outlined-blue": `border border-blue-400 text-blue-400 bg-white min-w-[80px]`,
+    "filled-blue": `border text-white bg-blue-500 min-w-[80px]`,
   };
   const sizeObj = {
     lg: "w-[546px] h-[40px] text-xl",
-    sm: "",
+    sm: "px-2 py-1",
+    xs: "px-2 py-[3px] leading-none text-sm",
   };
   const roundedObj = {
     full: "rounded-full",
@@ -45,26 +39,11 @@ function Button({
     semibold: "font-semibold",
     normal: "font-normal",
   };
-  const fontSizeObj = {
-    sm: "text-sm",
-    md: "text-base",
-    lg: "text-lg",
-  };
-  const paddingObj = {
-    "px-2_py-1": "px-2 py-1",
-    "px-2_py-[3px]": "px-2 py-[3px]",
-  };
-  const minWidthObj = {
-    "70px": "min-w-[70px]",
-  };
 
   return (
     <button
       type="button"
-      className={`${commonStyle} ${styleObj[styleType]} ${roundedObj[rounded]} ${sizeObj[size]} ${
-        fontWeightObj[fontWeight]
-      } ${fontSizeObj[fontSize]} ${paddingObj[padding]} ${minWidth && minWidthObj[minWidth]} ${
-        noLineHeight && "leading-none"
+      className={`${commonStyle} ${styleObj[styleType]} ${roundedObj[rounded]} ${sizeObj[size]} ${fontWeightObj[fontWeight]} 
       }`}
       onClick={onClick}
     >
