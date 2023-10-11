04c603ea8ee1d8d4bdcb7e3d2b9f26f63e049d27
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-11 20:54:55 +0900

    feat: RecordSearchBar SimpleDatePicker 컴포넌트 적용
    RecordSearchBar 컴포넌트에 SimpleDatePicker 컴포넌트를 적용했다.

--- src/components/molecules/RecordSearchBar/index.tsx
+++ src/components/molecules/RecordSearchBar/index.tsx
@@ -4,53 +4,40 @@ import { getCities } from "@/apis/district";
 import Button from "@/components/atoms/Button";
 import Dropdown from "@/components/atoms/Dropdown";
 import { useQuery } from "@tanstack/react-query";
-import { useCallback, useState } from "react";
+import { useState } from "react";
+import SimpleDatePicker from "../SimpleDatePicker";
 
 function RecordSearchBar(): JSX.Element {
-  const [searchBarState, setSearchBarState] = useState({ cityId: 0, start: "2022-02", end: "2022-02" });
+  const [cityId, setCityId] = useState(0);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
   const { data } = useQuery({
     queryKey: ["cityId"],
     queryFn: getCities,
   });
 
-  const handleOnChange = useCallback(
-    (event: React.ChangeEvent<HTMLSelectElement>, dropdownType: "cityId" | "start" | "end") => {
-      const newState = dropdownType === "cityId" ? parseInt(event.target.value, 10) : event.target.value;
-      setSearchBarState((prev) => ({
-        ...prev,
-        [dropdownType]: newState,
-      }));
-    },
-    [],
-  );
-
   const cities = data?.data?.response?.cities ?? [];
   return (
-    <div className="record-search-bar flex gap-4">
-      <Dropdown
-        placeholder=""
-        onChange={(e) => handleOnChange(e, "cityId")}
-        styleType="small"
-        options={[{ id: 0, name: "전체 지역" }, ...cities]}
-      />
-      <div className="period-dropdown flex gap-2 items-center">
+    <div className="record-search-bar flex gap-4 items-center">
+      <div>
         <Dropdown
           placeholder=""
-          onChange={(e) => handleOnChange(e, "start")}
+          onChange={(e) => {
+            setCityId(parseInt(e.target.value, 10));
+          }}
           styleType="small"
-          options={[{ id: 0, name: "1111년 11월" }]}
-        />
-        <span className="leading-none">~</span>
-        <Dropdown
-          placeholder=""
-          onChange={(e) => handleOnChange(e, "end")}
-          styleType="small"
-          options={[{ id: 0, name: "9999년 9월" }]}
+          options={[{ id: 0, name: "전체 지역" }, ...cities]}
         />
       </div>
-      <Button styleType="thunder" fontWeight="normal" size="sm" rounded="full">
-        조회
-      </Button>
+      <div className="period-dropdown flex gap-2 items-center">
+        <SimpleDatePicker value={startDate} setValue={setStartDate} />
+        <SimpleDatePicker minDate={startDate} value={endDate} setValue={setEndDate} />
+      </div>
+      <div>
+        <Button styleType="thunder" fontWeight="normal" size="sm" rounded="full">
+          조회
+        </Button>
+      </div>
     </div>
   );
 }
