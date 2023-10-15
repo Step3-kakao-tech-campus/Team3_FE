a44e0f9672bca8b573b4884b224585c1785cb4ee
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-15 21:40:58 +0900

    feat: ScoreboardTemplate RecordOverview 적용
    ScoreboardTemplate RecordOverview 적용

--- src/components/templates/ScoreboardTemplate/index.tsx
+++ src/components/templates/ScoreboardTemplate/index.tsx
@@ -1,13 +1,12 @@
-import RecordSummary from "@/components/atoms/RecordSummary";
 import RecordFilter from "@/components/molecules/RecordFilter";
+import RecordOverview from "@/components/molecules/RecordOverview";
 import RecordSearchBar from "@/components/molecules/RecordSearchBar";
 import RecordCardList from "@/components/organisms/RecordCardList";
 
-function ScoreboardTemplate(): JSX.Element {
+function ScoreboardTemplate({ userId }: { userId: number }): JSX.Element {
   return (
     <div className="scoreboard flex flex-col gap-5">
-      <h1 className="title text-2xl">{"김볼링"}님의 기록</h1>
-      <RecordSummary data={{ game: 20, average: 160, maximum: 180, minimum: 110 }} />
+      <RecordOverview userId={userId} />
       <h2 className="title text-xl">참여 기록</h2>
       <RecordFilter />
       <RecordSearchBar />
