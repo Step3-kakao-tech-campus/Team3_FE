1c43abbeedcdc6afc44587fa45931849eb982df6
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-15 21:28:48 +0900

    feat: RecordOverview 기능 구현
    RecordOverview 기능 구현

--- src/components/molecules/RecordOverview/index.tsx
+++ src/components/molecules/RecordOverview/index.tsx
@@ -1,7 +1,18 @@
 "use client";
 
-function RecordOverview() {
-  return <div>RecordOverview</div>;
+import { getRecord } from "@/apis/record";
+import RecordSummary from "@/components/atoms/RecordSummary";
+import { useQuery } from "@tanstack/react-query";
+
+function RecordOverview({ userId }: { userId: number }) {
+  const { data } = useQuery([`/api/users/${userId}/records`], () => getRecord(userId));
+  const userName = data?.data?.response?.name;
+  return (
+    <>
+      <h1 className="title text-2xl">{userName}님의 기록</h1>
+      <RecordSummary data={data?.data?.response} />
+    </>
+  );
 }
 
 export default RecordOverview;
