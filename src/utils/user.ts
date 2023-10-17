386bba0f0b335c4e88cdadfc3412c9ab288690a3
kjh302903 <kjh302903@gmail.com>
2023-10-17 23:39:16 +0900

    refactor: 쿠키에 저장하는 이메일 값 삭제
    쿠키에 저장하던 이메일 값 삭제

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -12,14 +12,12 @@ export const setLogin = async (email: string, token: string) => {
   const payload = getTokenPayload(token);
   const maxAge = 3600 * 168;
 
-  setCookie("email", email, { maxAge });
   setCookie("token", token, { maxAge });
   setCookie("exp", payload.exp, { maxAge });
   setCookie("userId", payload.sub, { maxAge });
 };
 
 export const deleteToken = async () => {
-  removeCookie("email");
   removeCookie("token");
   removeCookie("exp");
   removeCookie("userId");
