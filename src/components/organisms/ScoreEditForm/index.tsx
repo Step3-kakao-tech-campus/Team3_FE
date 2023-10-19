666d902f9acabb10090e8c215bc3e1f53b30e58a
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-20 02:13:36 +0900

    feat: ScoreEditForm handleUpdate, handleSubmit 삭제
    ScoreEditForm handleUpdate, handleSubmit 삭제

--- src/components/organisms/ScoreEditForm/index.tsx
+++ src/components/organisms/ScoreEditForm/index.tsx
@@ -7,15 +7,10 @@ import { useState } from "react";
 function ScoreEditForm() {
   const [formScores, setFormScores] = useState<ScoreData[]>([]);
 
-  const handleUpdate = (scoreFromInput: ScoreData) => {
-    const newScores = formScores.map((formScore) => (formScore.id === scoreFromInput.id ? scoreFromInput : formScore));
-    setFormScores(newScores);
-  };
-
   const handleAdd = () => {
     if (formScores.length >= 20) alert("최대 20 게임까지만 등록 가능합니다.");
     else {
-      const newScore = { id: Math.random(), scoreNum: 0, scoreImage: null, isValid: true, isNew: true };
+      const newScore = { id: Math.random(), scoreNum: 0, scoreImage: null, isNew: true };
       setFormScores([...formScores, newScore]);
     }
   };
@@ -25,31 +20,14 @@ function ScoreEditForm() {
     setFormScores(newScores);
   };
 
-  const handleSubmit = () => {
-    const isAllValid = formScores.every(({ isValid }) => isValid);
-    if (!isAllValid) alert("입력된 값들 중 유효하지 않은 값이 있습니다."); // toast로 대체
-    else {
-      // 서버로 전송
-      console.log("전송할 데이터:", formScores);
-    }
-  };
-
   return (
     <div>
       <h1>Score Form</h1>
       {formScores.map((formScore) => (
-        <ScoreInput
-          key={formScore?.id}
-          scoreData={formScore}
-          onUpdate={(newScore: ScoreData) => handleUpdate(newScore)}
-          onRemove={() => handleRemove(formScore?.id)}
-        />
+        <ScoreInput key={formScore?.id} scoreData={formScore} onRemove={() => handleRemove(formScore?.id)} />
       ))}
       <button type="button" onClick={handleAdd}>
         게임 추가
-      </button>
-      <button type="button" onClick={handleSubmit}>
-        저장
       </button>
     </div>
   );
