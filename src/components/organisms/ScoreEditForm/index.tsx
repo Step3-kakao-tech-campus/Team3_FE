300936374860e5fbd7e11888f870c1749de42c33
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-20 12:48:18 +0900

    fix: ScoreEditForm initialScores 예외 처리
    ScoreEditForm initialScores 예외 처리

--- src/components/organisms/ScoreEditForm/index.tsx
+++ src/components/organisms/ScoreEditForm/index.tsx
@@ -9,28 +9,29 @@ interface Props {
 }
 
 function ScoreEditForm({ initialScoresData }: Props) {
-  const initialScores = initialScoresData.map((score) => {
-    return { ...score, isNew: false };
-  });
+  const initialScores: ScoreData[] =
+    initialScoresData?.map((score) => {
+      return { ...score, isNew: false };
+    }) || [];
   const [formScores, setFormScores] = useState<ScoreData[]>(initialScores);
 
   const handleAdd = () => {
-    if (formScores.length >= 20) alert("최대 20 게임까지만 등록 가능합니다.");
+    if (formScores?.length >= 20) alert("최대 20 게임까지만 등록 가능합니다.");
     else {
       const newScore = { id: Math.random(), scoreNum: 0, scoreImage: null, isNew: true };
-      setFormScores([...formScores, newScore]);
+      setFormScores((prev) => [...prev, newScore]);
     }
   };
 
   const handleRemove = (id: ScoreData["id"]) => {
-    const newScores = formScores.filter((formScore) => formScore.id !== id);
+    const newScores = formScores?.filter((formScore) => formScore.id !== id);
     setFormScores(newScores);
   };
 
   return (
     <div>
       <h1>Score Form</h1>
-      {formScores.map((formScore) => (
+      {formScores?.map((formScore) => (
         <ScoreInput key={formScore?.id} scoreData={formScore} onRemove={() => handleRemove(formScore?.id)} />
       ))}
       <button type="button" onClick={handleAdd}>
