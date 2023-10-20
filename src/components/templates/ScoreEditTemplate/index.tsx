8dcdbf95f9249cb89a220cf42f2eb94ae421eaee
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-20 11:34:14 +0900

    feat: ScoreEditTemplate 점수 조회 api 적용
    ScoreEditTemplate 점수 조회 api 적용

--- src/components/templates/ScoreEditTemplate/index.tsx
+++ src/components/templates/ScoreEditTemplate/index.tsx
@@ -1,9 +1,15 @@
-import ScoreEditForm from "@/components/organisms/ScoreEditForm";
+"use client";
 
-function ScoreEditTemplate() {
+import { getScore } from "@/apis/record";
+import ScoreEditForm from "@/components/organisms/ScoreEditForm";
+import { useQuery } from "@tanstack/react-query";
+
+function ScoreEditTemplate({ postId }: { postId: number }) {
+  const { data } = useQuery([`/api/posts/${postId}/scores`], () => getScore(postId));
+  const scores = data?.data?.response?.scores;
   return (
     <div className="score-edit-template">
-      <ScoreEditForm />
+      <ScoreEditForm initialScoresData={scores} />
     </div>
   );
 }
