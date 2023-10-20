9d960a7d0b7e5c14fb93392db1b3996223fdd550
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-20 11:38:07 +0900

    style: ScoreInput formData file 타입 변환
    ScoreInput formData file 타입 변환

--- src/components/molecules/ScoreInput/index.tsx
+++ src/components/molecules/ScoreInput/index.tsx
@@ -79,7 +79,7 @@ function ScoreInput({ scoreData, onRemove }: Props) {
       const isImageModified = selectedFile !== scoreData.scoreImage;
       const formData: { score?: number; image?: File } = {};
       if (isScoreModified) formData.score = scoreValue;
-      if (isImageModified) formData.image = selectedFile;
+      if (isImageModified) formData.image = selectedFile as File | undefined;
       putEditScore({ postId, scoreId, formData });
     } else if (isValid && !isNew && !isModified) {
       setIsEditing(false);
