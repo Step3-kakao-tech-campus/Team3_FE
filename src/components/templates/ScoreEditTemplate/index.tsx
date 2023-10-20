3f9221e2d05fb767a8702cd8d028698bc6c64601
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-20 17:15:18 +0900

    feat: ScoreEditTemplate 유저 이름, 프로필 이미지 표현
    ScoreEditTemplate 유저 이름, 프로필 이미지 표현

--- src/components/templates/ScoreEditTemplate/index.tsx
+++ src/components/templates/ScoreEditTemplate/index.tsx
@@ -1,14 +1,25 @@
 "use client";
 
+import { getMyProfile } from "@/apis/profile";
 import { getScore } from "@/apis/record";
+import CircularProfileImage from "@/components/atoms/CircularProfileImage";
 import ScoreEditForm from "@/components/organisms/ScoreEditForm";
 import { useQuery } from "@tanstack/react-query";
 
 function ScoreEditTemplate({ postId }: { postId: number }) {
-  const { data } = useQuery([`/api/posts/${postId}/scores`], () => getScore(postId));
-  const scores = data?.data?.response?.scores;
+  const { data: scoreData } = useQuery([`/api/posts/${postId}/scores`], () => getScore(postId));
+  const { data: profileData } = useQuery(["/api/users/mine"], getMyProfile);
+  const scores = scoreData?.data?.response?.scores;
+  const userName = profileData?.data?.response?.name;
+  const profileImage = profileData?.data?.response?.profileImage;
   return (
     <div className="score-edit-template">
+      {profileData?.data?.response && (
+        <div className="score-edit-title flex gap-2 text-2xl items-center leading-none">
+          <CircularProfileImage src={profileImage} styleType="lg" />
+          <h1>{`${userName}님의 점수`}</h1>
+        </div>
+      )}
       <ScoreEditForm initialScoresData={scores} />
     </div>
   );
