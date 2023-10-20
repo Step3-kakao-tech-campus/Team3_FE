29540c36313843c7b9a16615af1ae4fef52db62e
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-21 02:22:41 +0900

    feat: StarRatingPage 생성
    StarRatingPage 생성

---
+++ src/app/@modal/(.)star_rating/page.tsx
@@ -0,0 +1,12 @@
+import Modal from "@/components/atoms/Modal";
+import StarRatingTemplate from "@/components/templates/StarRatingTemplate";
+
+function StarRatingPage() {
+  return (
+    <Modal>
+      <StarRatingTemplate />
+    </Modal>
+  );
+}
+
+export default StarRatingPage;
