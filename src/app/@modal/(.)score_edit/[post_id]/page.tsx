b2423288c602bcbcbc1d3d13b274d917295dacd6
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-20 11:33:41 +0900

    feat: ScoreEditPage param prop 타입 설정
    ScoreEditPage param prop 타입 설정

--- src/app/@modal/(.)score_edit/[post_id]/page.tsx
+++ src/app/@modal/(.)score_edit/[post_id]/page.tsx
@@ -1,10 +1,16 @@
 import Modal from "@/components/atoms/Modal";
 import ScoreEditTemplate from "@/components/templates/ScoreEditTemplate";
 
-function ScoreEditPage() {
+interface Props {
+  params: {
+    post_id: string;
+  };
+}
+
+function ScoreEditPage({ params }: Props) {
   return (
     <Modal>
-      <ScoreEditTemplate />
+      <ScoreEditTemplate postId={parseInt(params.post_id, 10)} />
     </Modal>
   );
 }
