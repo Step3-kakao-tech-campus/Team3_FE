83106ce62c63dd42666f5e2a6b537a2cd9b0b9f1
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-21 02:22:15 +0900

    feat: StarRatingTemplate 컴포넌트 제작
    StarRatingTemplate 컴포넌트 제작

---
+++ src/components/templates/StarRatingTemplate/index.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { getProfileById } from "@/apis/profile";
+import postRating from "@/apis/rating";
+import CircularProfileImage from "@/components/atoms/CircularProfileImage";
+import StarButtons from "@/components/atoms/StarButtons";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+
+function StarRatingTemplate() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const applicantId = parseInt(searchParams.get("applicantId") as string, 10);
+  const targetId = parseInt(searchParams.get("targetId") as string, 10);
+
+  const [star, setStar] = useState(0);
+
+  const { data } = useQuery([`/api/users/${targetId}`], () => getProfileById(targetId));
+  const userName = data?.data?.response?.name;
+  const userImage = data?.data?.response?.profileImage;
+
+  const { mutate: postStarRating } = useMutation(postRating, {
+    onSuccess: () => {
+      alert("등록이 완료되었습니다.");
+      router.back();
+    },
+    onError: () => {
+      alert("요청이 실패하였습니다.");
+    },
+  });
+
+  return (
+    <div className="star-rating-template flex flex-col gap-3">
+      <div className="flex items-center gap-2 text-xl">
+        <CircularProfileImage src={userImage} />
+        <h1>{userName && `${userName}님의`}</h1>
+      </div>
+      <div>
+        <span className="ml-1">매너점수</span>
+        <StarButtons star={star} setStar={setStar} />
+      </div>
+      <button
+        type="button"
+        className="w-full p-[3px] bg-thunder text-white rounded-full"
+        onClick={() => postStarRating({ applicantId, targetId, rating: star })}
+      >
+        확인
+      </button>
+    </div>
+  );
+}
+
+export default StarRatingTemplate;
