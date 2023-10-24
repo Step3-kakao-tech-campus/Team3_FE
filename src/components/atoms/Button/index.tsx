51c30dc72fc95f53e955d2a9d023e9d79d15b84c
kjh302903 <kjh302903@gmail.com>
2023-10-24 10:22:38 +0900

    refactor: 버튼 md 사이즈 추가
    버튼 md 사이즈 추가

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,7 +1,7 @@
 export interface ButtonProps {
   styleType: "white" | "thunder" | "outlined-gray" | "outlined-orange" | "outlined-blue" | "filled-blue" | "filled-red";
   rounded: "full" | "md";
-  size: "lg" | "sm" | "xs";
+  size: "lg" | "md" | "sm" | "xs";
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   children: React.ReactNode;
   fontWeight?: "bold" | "semibold" | "normal";
@@ -21,6 +21,7 @@ function Button({ styleType, rounded, size, onClick, children, fontWeight = "bol
   const sizeObj = {
     lg: "w-[546px] h-[40px] text-xl",
     sm: "px-2 py-1",
+    md: "px-2 py-1.5 min-w-[100px]",
     xs: "px-2 py-[3px] leading-none text-sm min-w-[80px]",
   };
   const roundedObj = {
