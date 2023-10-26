3c7e9cfc60f690cfba5b524d4a0b2d5aa46b3548
kjh302903 <kjh302903@gmail.com>
2023-10-26 10:17:02 +0900

    feat: 메시지 삭제 기능 추가
    메시지 hover시 x 버튼 뜨게 했음
    x 버튼 누를시 모달로 삭제 여부 물어봄
    확인시 삭제

--- src/components/molecules/Message/index.tsx
+++ src/components/molecules/Message/index.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import { TiDeleteOutline } from "react-icons/ti";
 import CircularProfileImage from "@/components/atoms/CircularProfileImage";
 import { MessageType } from "@/types/message";
 import { formatDateToMessageTime } from "@/utils/formatDateToString";
-import React from "react";
+import React, { useState } from "react";
+import { deleteMessages } from "@/apis/message";
+import useMutateWithQueryClient from "@/hooks/useMutateWithQueryClient";
+import { useParams } from "next/navigation";
+import ReconfirmModal from "../SemiModal/ReconfirmModal";
 
 interface Props {
   message: MessageType;
@@ -11,12 +18,31 @@ interface Props {
 }
 
 function Message({ message, isProfile, opponentUserName, opponentUserProfileImage }: Props): JSX.Element {
+  const params = useParams();
+  const id = parseInt(params.opponent_user_id as string, 10);
+  const [isHovered, setIsHovered] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const { mutate, queryClient } = useMutateWithQueryClient(deleteMessages);
+
   let style;
   if (message.isReceive) {
     style = "bg-[#FDAA49] rounded-br-md";
   } else {
     style = "bg-[#FDF1C4] rounded-bl-md";
   }
+
+  const handleComplete = () => {
+    mutate(
+      { id: message.id },
+      {
+        onSuccess: () => {
+          queryClient.invalidateQueries([`/api/messages/opponents/${id}`, id]);
+          setModalOpen(false);
+        },
+      },
+    );
+  };
 
   return (
     <div className={`relative flex ${!message.isReceive && "flex-row-reverse"}`}>
@@ -28,14 +54,44 @@ function Message({ message, isProfile, opponentUserName, opponentUserProfileImag
           <span className="absolute top-[-24px] left-14 text-sm font-medium">{opponentUserName}</span>
         </>
       )}
-      <pre
-        className={`inline-block max-w-[420px] mx-2 p-2 rounded-t-md ${style} whitespace-pre-wrap break-all ${
-          message.isReceive && "ml-14"
-        }`}
+      <div
+        className={`flex ${!message.isReceive && "flex-row-reverse"}`}
+        onMouseEnter={() => {
+          if (!message.isReceive) setIsHovered(true);
+        }}
+        onMouseLeave={() => {
+          if (!message.isReceive) setIsHovered(false);
+        }}
       >
-        {message.content}
-      </pre>
-      <span className="text-xs mt-auto">{formatDateToMessageTime(message.time)}</span>
+        <pre
+          className={`inline-block max-w-[420px] mx-2 p-2 rounded-t-md ${style} whitespace-pre-wrap break-all ${
+            message.isReceive && "ml-14"
+          }`}
+        >
+          {message.content}
+        </pre>
+        {isHovered ? (
+          <button
+            type="button"
+            className="mt-auto ml-[34px] p-1 bg-amber-200 rounded-md hover:brightness-95"
+            onClick={() => {
+              setModalOpen(true);
+            }}
+          >
+            <TiDeleteOutline className="w-5 h-5" />
+          </button>
+        ) : (
+          <span className="text-xs mt-auto">{formatDateToMessageTime(message.time)}</span>
+        )}
+      </div>
+      {modalOpen && (
+        <ReconfirmModal
+          handleComplete={handleComplete}
+          handleCancel={() => {
+            setModalOpen(false);
+          }}
+        />
+      )}
     </div>
   );
 }
