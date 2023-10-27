6c20b6ee49e466a5b3b377e003d8466f0e55e3be
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-27 12:21:17 +0900

    fix: 별점주기 api 주소 수정
    별점주기 api 주소 수정

--- src/apis/rating.ts
+++ src/apis/rating.ts
@@ -1,16 +1,17 @@
 import client from "./instance";
 
 interface Param {
+  postId: number;
   applicantId: number;
   targetId: number;
   rating: number;
 }
 
-export default async function postRating({ applicantId, targetId, rating }: Param) {
+export default async function postRating({ postId, applicantId, targetId, rating }: Param) {
   if (!applicantId) throw new Error("applicantId가 유효하지 않습니다.");
   if (!targetId) throw new Error("targetId가 유효하지 않습니다.");
   if (!rating) throw new Error("rating가 유효하지 않습니다.");
-  const response = await client.post(`/api/applicants/${applicantId}/rating`, {
+  const response = await client.post(`/api/posts/${postId}/applicants/${applicantId}/rating`, {
     targetId,
     rating,
   });
