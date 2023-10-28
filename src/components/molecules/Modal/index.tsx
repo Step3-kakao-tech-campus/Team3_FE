d0e2ba2d8d8d294a0499651313d4998da3b29e81
kjh302903 <kjh302903@gmail.com>
2023-10-28 13:28:09 +0900

    refactor: 신청자 확인모달 재구현
    신청자 확인 모달 재구현

---
+++ src/components/molecules/Modal/index.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from "react";
+import { MdClose } from "react-icons/md";
+
+interface Props {
+  children: React.ReactNode;
+  noPadding?: boolean;
+  onDismiss: () => void;
+}
+
+function ModalWrapper({ children, noPadding, onDismiss }: Props): JSX.Element {
+  return (
+    <div className="fixed z-50 left-0 right-0 top-0 bottom-0 flex items-center justify-center ">
+      <div
+        className={`relative bg-white rounded-2xl border-[#868484] shadow-2xl overflow-clip ${
+          noPadding ? "p-0" : "p-6"
+        }`}
+      >
+        <button
+          type="button"
+          className="absolute top-2 right-2 text-gray-400 hover:text-gray-600 text-2xl font-bold rounded-full w-7 h-7 flex items-center justify-center focus:outline-none"
+          onClick={onDismiss}
+          aria-label="Modal Close"
+        >
+          <MdClose />
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default ModalWrapper;
