32ce6435fb15d49d684f6b120ced150e85fb75e7
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-10-30 15:39:14 +0900

    refactor: ScoreEditForm useEffect 로직 변경
    ScoreEditForm useEffect 로직 변경

--- src/components/organisms/ScoreEditForm/index.tsx
+++ src/components/organisms/ScoreEditForm/index.tsx
@@ -7,10 +7,11 @@ import { useEffect, useState } from "react";
 import { MdAddCircleOutline } from "react-icons/md";
 
 interface Props {
+  postId: number;
   initialScoresData: Score[];
 }
 
-function ScoreEditForm({ initialScoresData }: Props) {
+function ScoreEditForm({ postId, initialScoresData }: Props) {
   const newScore = { id: Math.random(), scoreNum: 0, scoreImage: null, isNew: true };
 
   const [formScores, setFormScores] = useState<ScoreData[]>([]);
@@ -19,11 +20,8 @@ function ScoreEditForm({ initialScoresData }: Props) {
       return { ...score, isNew: false };
     });
     setFormScores((prev) => {
-      const mergedScores = [...newFormScores, ...prev];
-      const uniqueScores = [...new Set(mergedScores.map((score) => score.id))].map(
-        (id) => mergedScores.find((score) => score.id === id) as ScoreData,
-      );
-      return uniqueScores.length ? uniqueScores : [newScore];
+      const mergedScores = [...newFormScores, ...prev.filter((score) => score.id < 1)];
+      return mergedScores.length ? mergedScores : [newScore];
     });
   }, [initialScoresData]);
 
@@ -43,7 +41,12 @@ function ScoreEditForm({ initialScoresData }: Props) {
   return (
     <div className="score-edit-form flex flex-col gap-2 min-w-[350px]">
       {formScores?.map((formScore) => (
-        <ScoreInput key={formScore?.id} scoreData={formScore} onRemove={() => handleRemove(formScore?.id)} />
+        <ScoreInput
+          key={formScore?.id}
+          postId={postId}
+          scoreData={formScore}
+          onRemove={() => handleRemove(formScore?.id)}
+        />
       ))}
       <button type="button" onClick={handleAdd}>
         <span className="inline-flex gap-1 text-neutral-500 items-center">
