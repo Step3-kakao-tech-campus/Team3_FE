7f2d94dadea57def0177a424c837567fb37cdc61
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-11-01 11:05:56 +0900

    feat: 이메일 인증 로딩 페이지 WhiteContainer 적용
    이메일 인증 로딩 페이지 WhiteContainer 적용

--- src/app/email-verification/loading.tsx
+++ src/app/email-verification/loading.tsx
@@ -1,10 +1,13 @@
 import LoadingSpinner from "@/components/atoms/LoadingSpinner";
+import WhiteContainer from "@/components/atoms/WhiteContainer";
 
 function EmailVerificationLoading() {
   return (
-    <div className="flex justify-center items-center h-[80vh]">
-      <LoadingSpinner styleType="xl" />
-    </div>
+    <WhiteContainer>
+      <div className="flex justify-center items-center h-[80vh]">
+        <LoadingSpinner styleType="xl" />
+      </div>
+    </WhiteContainer>
   );
 }
 
