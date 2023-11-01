aac56f19669c88f46230a994303a004aa03a2f02
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-11-01 10:32:40 +0900

    fix: next config 이미지 도메인 등록
    next config 이미지 도메인 등록

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bungaebowling-img-s3.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
