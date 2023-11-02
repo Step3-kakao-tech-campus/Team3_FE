aa3df939a8e0cc0d5db446dde36e0cb1667c8969
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-11-02 14:38:26 +0900

    design: 메일 인증 에러 페이지 UI 개발
    메일 인증 에러 페이지 UI 개발

--- src/app/email-verification/error.tsx
+++ src/app/email-verification/error.tsx
@@ -2,11 +2,21 @@
 
 export default function Error({ reset }: { reset: () => void }) {
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button type="button" onClick={() => reset()}>
-        Try again
-      </button>
+    <div className="email-verification-error min-h-[60vh] flex flex-col justify-evenly">
+      <div className="flex flex-col items-center gap-8">
+        <h2 className="text-3xl">오류가 발생했습니다.</h2>
+        <div className="email-verification-error flex flex-col items-center gap-3">
+          <p className="text-xl text-neutral-500">메일 인증 요청 중 오류가 발생했습니다.</p>
+          <p className="text-xl text-neutral-500">아래의 버튼을 눌러 다시 시도해 주세요.</p>
+        </div>
+        <button
+          type="button"
+          className="border bg-thunder px-4 py-2 text-white rounded-2xl hover:brightness-95"
+          onClick={() => reset()}
+        >
+          다시 시도
+        </button>
+      </div>
     </div>
   );
 }
