7a002aa091f4fcb0992575334363e378cd3c54a5
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-11-02 16:06:58 +0900

    feat: 메일 인증 페이지 기능 구현
    메일 인증 페이지 기능 구현

--- src/app/email-verification/page.tsx
+++ src/app/email-verification/page.tsx
@@ -1,12 +1,23 @@
 "use client";
 
-import useIsMounted from "@/hooks/useMounted";
+import { useMutation } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { postConfirmVerificationEmail } from "@/apis/email";
+import VerificationEmailTemplate from "@/components/templates/EmailVerificationTemplate/VerificationEmailTemplate";
+import { useSearchParams } from "next/navigation";
 import EmailVerificationLoading from "./loading";
 
 function EmailVerificationPage() {
-  const { isMounted } = useIsMounted();
-  if (!isMounted) return <EmailVerificationLoading />;
-  return <div>EmailVerificationPage</div>;
+  const searchParam = useSearchParams();
+  const token = searchParam.get("token") as string;
+  const { mutate, isSuccess } = useMutation(postConfirmVerificationEmail, { useErrorBoundary: true });
+
+  useEffect(() => {
+    mutate({ token });
+  }, []);
+
+  if (!isSuccess) return <EmailVerificationLoading />;
+  return <VerificationEmailTemplate />;
 }
 
 export default EmailVerificationPage;
