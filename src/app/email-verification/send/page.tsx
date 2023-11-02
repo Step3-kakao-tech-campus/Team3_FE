2161b65b6255625dfcfbea2e198b804e0f269dbc
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-11-02 14:08:16 +0900

    feat: 인증메일 발송 페이지 마운트 시 인증메일 발송 요청 처리
    인증메일 발송 페이지 마운트 시 인증메일 발송 요청 처리

--- src/app/email-verification/send/page.tsx
+++ src/app/email-verification/send/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import useIsMounted from "@/hooks/useMounted";
+import SendVerificationEmailTemplate from "@/components/templates/EmailVerificationTemplate/SendVerificationEmailTemplate";
+import { useEffect } from "react";
+import { postSendVerificationEmail } from "@/apis/email";
+import { useMutation } from "@tanstack/react-query";
 import EmailVerificationLoading from "../loading";
 
 function EmailSendPage() {
-  const { isMounted } = useIsMounted();
-  if (!isMounted) return <EmailVerificationLoading />;
-  return <div>EmailSendPage</div>;
+  const { mutate, isSuccess } = useMutation(postSendVerificationEmail, { useErrorBoundary: true });
+
+  useEffect(mutate, []);
+
+  if (!isSuccess) return <EmailVerificationLoading />;
+  return <SendVerificationEmailTemplate />;
 }
 
 export default EmailSendPage;
