a2d14a2d450bd978ea0da7b05208728daa9a18e9
kjh302903 <kjh302903@gmail.com>
2023-11-04 21:18:00 +0900

    feat: 볼링장 찾기 페이지 생성
    볼링장 찾기 페이지 생성

---
+++ src/app/search/page.tsx
@@ -0,0 +1,12 @@
+import BackArrowContainer from "@/components/atoms/BackArrowContainer";
+import SearchTemplate from "@/components/templates/SearchTemplate";
+
+function SearchHome() {
+  return (
+    <BackArrowContainer>
+      <SearchTemplate />
+    </BackArrowContainer>
+  );
+}
+
+export default SearchHome;
