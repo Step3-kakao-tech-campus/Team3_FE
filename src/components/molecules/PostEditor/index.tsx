5f28a8021d5342c490b704a971b2b3adc8b8a52d
kjh302903 <kjh302903@gmail.com>
2023-11-06 10:38:45 +0900

    refactor: 게시글 재확인 로직 리팩토링
    로직을 PostEditor 안으로 옮김

--- src/components/molecules/PostEditor/index.tsx
+++ src/components/molecules/PostEditor/index.tsx
@@ -1,32 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlineDelete, MdOutlineEdit } from "react-icons/md";
 import { useRouter } from "next/navigation";
+import useToast from "@/hooks/useToast";
+import { useMutation } from "@tanstack/react-query";
+import { deletePost } from "@/apis/posts";
+import ReconfirmModal from "../SemiModal/ReconfirmModal";
 
 interface Props {
   id: number;
-  handleOnClick: () => void;
 }
 
-function PostEditor({ id, handleOnClick }: Props) {
+function PostEditor({ id }: Props) {
   const router = useRouter();
 
+  const [reconfirmModalOpen, setReconfirmModalOpen] = useState(false);
+
+  const { mutate } = useMutation(deletePost);
+  const { addSuccessToast } = useToast();
+
+  const handleDelete = () => {
+    mutate(
+      { id },
+      {
+        onSuccess: () => {
+          setReconfirmModalOpen(false);
+          router.push("/");
+          addSuccessToast("성공적으로 삭제 되었습니다.");
+        },
+        onError: (error) => {
+          console.log(error);
+        },
+      },
+    );
+  };
+
   return (
-    <div className="flex text-neutral-500 gap-3">
-      <button
-        type="button"
-        className="flex items-center cursor-pointer"
-        onClick={() => {
-          router.push(`/post/${id}/edit`);
-        }}
-      >
-        <MdOutlineEdit />
-        수정
-      </button>
-      <button type="button" className="flex items-center cursor-pointer" onClick={handleOnClick}>
-        <MdOutlineDelete />
-        삭제
-      </button>
-    </div>
+    <>
+      <div className="flex text-neutral-500 gap-3">
+        <button
+          type="button"
+          className="flex items-center cursor-pointer"
+          onClick={() => {
+            router.push(`/post/${id}/edit`);
+          }}
+        >
+          <MdOutlineEdit />
+          수정
+        </button>
+        <button
+          type="button"
+          className="flex items-center cursor-pointer"
+          onClick={() => {
+            setReconfirmModalOpen(true);
+          }}
+        >
+          <MdOutlineDelete />
+          삭제
+        </button>
+      </div>
+      {reconfirmModalOpen && (
+        <ReconfirmModal
+          target="게시글을"
+          handleComplete={handleDelete}
+          handleCancel={() => {
+            setReconfirmModalOpen(false);
+          }}
+        />
+      )}
+    </>
   );
 }
 
