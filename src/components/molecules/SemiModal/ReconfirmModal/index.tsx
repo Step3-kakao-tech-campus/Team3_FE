b0b4c52b2341bd9e6edbdb4c9a966075254e0a44
kjh302903 <kjh302903@gmail.com>
2023-11-07 20:11:17 +0900

    feat: 삭제 모달 모바일 UI 적용
    삭제 모달 모바일 UI 적용

--- src/components/molecules/SemiModal/ReconfirmModal/index.tsx
+++ src/components/molecules/SemiModal/ReconfirmModal/index.tsx
@@ -11,7 +11,7 @@ function ReconfirmModal({ handleComplete, handleCancel, target }: Props): JSX.El
   return (
     <SemiModalContainer>
       <div className="h-full">
-        <div className="h-[75%] p-10 border-b border-slate-300">
+        <div className="h-[75%] p-10 border-b border-slate-300 md:p-6">
           <h2 className="mb-4 text-lg font-bold">{target.slice(0, -1)} 삭제</h2>
           <p>{target} 삭제하시겠습니까?</p>
         </div>
