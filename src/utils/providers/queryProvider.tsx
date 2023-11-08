8384268059efa5426047cda4939cf1c6289c6f3f
kjh302903 <kjh302903@gmail.com>
2023-11-08 17:22:20 +0900

    refactor: queryclient 분리
    queryClient 분리

--- src/utils/providers/queryProvider.tsx
+++ src/utils/providers/queryProvider.tsx
@@ -1,26 +1,15 @@
 "use client";
 
 import React from "react";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./queryClient";
 
 type Props = {
   children: React.ReactNode;
 };
 
 function QueryProvider({ children }: Props) {
-  const [client] = React.useState(
-    new QueryClient({
-      defaultOptions: {
-        // react-query 전역 설정
-        queries: {
-          refetchOnWindowFocus: false,
-          retry: false,
-        },
-      },
-    }),
-  );
-
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
 
 export default QueryProvider;
